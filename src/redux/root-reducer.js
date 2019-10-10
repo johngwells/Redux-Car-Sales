ae1602ccbfeceda552d904e32eb796a368c39f75
John Wells <molekewl2017@gmail.com>
2019-10-10 05:23:25 -0700

    add car feature reducer to the root reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 // import reducers here
+import carFeaturesReducer from './car-features';
 
 export default combineReducers({
   // reducers here
+  car: carFeaturesReducer
 });
